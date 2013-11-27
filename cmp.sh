ae6f40f6d1d018e24636ff855a56ca27bfbadf8e
zly <dear531@21cn.com>
2013-11-27 17:29:43 +0800

    mxd site update shell script

---
+++ cmp.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+file_a=qunmenber2013.11.27.txt
+file_b=mxtdb2013.11.27.txt
+member_a=`cat $file_a |awk '{print $1}'`
+member_b=`cat $file_b |awk '{print $2}'`
+#echo $member_a;
+#echo $member_b;
+for i in $member_a
+do
+	flag=0;
+	for j in $member_b
+	do
+		if [ "$i" == "$j" ];
+		then
+#			echo $j
+			flag=1;
+			break;
+		fi
+	done
+	if [ $flag -eq 0 ];
+	then
+#		echo $i;
+		tmp+=" $i";
+	fi
+#	echo $i;
+done
+#echo $tmp;
+for k in $tmp
+do
+	cat $file_a | awk -v n=$k '{if($1==n) print $0}'
+#	echo $k;
+done
